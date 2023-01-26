c43b54a543c5900e2c54988ccda5b41be6531ea5
JustTuri <arturogcorral@comcast.net>
2023-01-26 14:41:33 -0500

    Implemented algorithm on static data

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,76 @@
-currency_exchange = [[1, 2, 3],
-                     [4, 5, 6],
-                     [7, 8, 9]]
+from typing import Tuple, List
+from math import log
 
-print(currency_exchange[1][2])
+exchange_rates = [
+    [1, 0.23, 0.25, 16.43, 18.21, 4.94],
+    [4.34, 1, 1.11, 71.40, 79.09, 21.44],
+    [3.93, 0.90, 1, 64.52, 71.48, 19.37],
+    [0.061, 0.014, 0.015, 1, 1.11, 0.30],
+    [0.055, 0.013, 0.014, 0.90, 1, 0.27],
+    [0.20, 0.047, 0.052, 3.33, 3.69, 1],
+]
+
+currencies = ('PLN', 'EUR', 'USD', 'RUB', 'INR', 'MXN')
+
+#Log + negate the currencies for arbitrage
+
+# By taking the natural logarithm of these rates,
+# we can convert these multiplicative changes into additive changes,
+# which can help to simplify the analysis of the data.
+# Logging the rates can also help to identify patterns and trends that may not be immediately apparent when working with the raw data.
+
+# By negating the exchange rates, we can find negative cycles. Which is an arbitrage opportunity
+
+def negate_logarithm_converter(exchange_rates):
+    col = len(exchange_rates)
+    row = len(exchange_rates[0])
+
+    for i in range(col):
+        for j in range(row):
+            exchange_rates[i][j] = -log(exchange_rates[i][j])
+
+    #print(exchange_rates)
+    return exchange_rates
+
+
+
+def bellman_ford_arbitrage(currency_tuple: tuple, rates_matrix: Tuple[Tuple[float, ...]]):
+    ''' Calculates arbitrage situations and prints out the details of this calculations'''
+
+    trans_graph = negate_logarithm_converter(rates_matrix)
+
+    # Pick any source vertex -- we can run Bellman-Ford from any vertex and get the right result
+
+    source = 0
+    n = len(trans_graph)
+    min_dist = [float('inf')] * n
+
+    pre = [-1] * n
+    
+    min_dist[source] = source
+
+    # 'Relax edges |V-1| times'
+    for _ in range(n-1):
+        for source_curr in range(n):
+            for dest_curr in range(n):
+                if min_dist[dest_curr] > min_dist[source_curr] + trans_graph[source_curr][dest_curr]:
+                    min_dist[dest_curr] = min_dist[source_curr] + trans_graph[source_curr][dest_curr]
+                    pre[dest_curr] = source_curr
+
+    # if we can still relax edges, then we have a negative cycle
+    for source_curr in range(n):
+        for dest_curr in range(n):
+            if min_dist[dest_curr] > min_dist[source_curr] + trans_graph[source_curr][dest_curr]:
+                # negative cycle exists, and use the predecessor chain to print the cycle
+                print_cycle = [dest_curr, source_curr]
+                # Start from the source and go backwards until you see the source vertex again or any vertex that already exists in print_cycle array
+                while pre[source_curr] not in print_cycle:
+                    print_cycle.append(pre[source_curr])
+                    source_curr = pre[source_curr]
+                print_cycle.append(pre[source_curr])
+                print("Arbitrage Opportunity:")
+                print(" --> ".join([currencies[p] for p in print_cycle[::-1]]) + '\n')
+
+
+if __name__ == "__main__":
+    bellman_ford_arbitrage(currencies, exchange_rates)
